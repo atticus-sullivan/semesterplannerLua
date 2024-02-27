179e27771fa3675ccb97e37fb7f27bcd027f8c2e
Lukas <oss.heindl+git@protonmail.com>
2024-02-27 13:11:24 +0100

    remove .link.md

--- build.lua
+++ build.lua
@@ -4,3 +4,4 @@ unpackexe = "luatex"
 
 installfiles = {"*.lua", "*.sty"}
 sourcefiles = {"*.dtx", "*.ins", "semesterplannerLua_calendar.lua", "semesterplannerLua_dmenu.lua", "semesterplannerLua_timetable.lua"}
+excludefiles = {".link.md", "*~","build.lua","config-*.lua"}
